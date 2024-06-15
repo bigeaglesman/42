55f480e4ed3e8c812579af827c029dbc7022ff3a
Wsl Home <youngho@DESKTOP-8UARNTE.localdomain>
2024-06-16 00:47:09 +0900

    1

---
+++ cursus/circle3/philosopher/philo/includes/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youngho <youngho@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 20:03:06 by youngho           #+#    #+#             */
+/*   Updated: 2024/06/15 21:58:32 by youngho          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+typedef struct s_arg
+{
+    
+}t_arg;
+
+
+#endif
