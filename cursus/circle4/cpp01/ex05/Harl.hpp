0a87ef1000e7c1c1869b9866fa88a962f1e26195
ycho <choyh1221@naver.com>
2025-02-12 12:11:38 +0900

    0212

--- cursus/circle4/cpp01/ex05/Harl.hpp
+++ cursus/circle4/cpp01/ex05/Harl.hpp
@@ -6,14 +6,13 @@
 /*   By: ycho2 <ycho2@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 20:04:42 by ycho2             #+#    #+#             */
-/*   Updated: 2025/01/18 22:12:38 by ycho2            ###   ########.fr       */
+/*   Updated: 2025/02/12 12:11:01 by ycho2            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef HARL_HPP
 # define HARL_HPP
 
-# include <string>
 # include <iostream>
 
 class Harl
